b150b8885730692e0a671e4a6fe1a319664ebfcc
Pasidu22 <sakurangapasindu2002@gmail.com>
2026-08-01 08:11:07 +0530

    Add favicon using site logo

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
